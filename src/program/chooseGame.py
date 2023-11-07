8d02f7a7c32e3c2630c35bde47186585a796c8c2
Leandro de Oliveira Custódio <99210769+TheLeleo@users.noreply.github.com>
2023-11-07 15:10:16 -0300

    title v1

--- src/program/chooseGame.py
+++ src/program/chooseGame.py
@@ -1,18 +1,28 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import font
 
-#window
+# Janela principal
 window = tk.Tk()
 window.geometry('600x400')
 window.title('Lelis')
 
-#menu
-menu = tk.Menu(window)
 
-#sub menu
-file_menu = tk.Menu(menu)
+# Frame para a "div"
+div_frame = tk.Frame(window, bg='white', relief='solid', highlightbackground="red", highlightthickness=2)
+div_frame.pack(fill='x', ipadx=900, ipady=12)  # Define o lado esquerdo para a "div"
 
-window.configure(menu = menu)
+# Estilo de fonte em negrito
+bold_font = font.nametofont("TkDefaultFont")
+bold_font.configure(weight='bold')
 
-#run
+# Label dentro da "div"
+test = tk.Label(div_frame, text="SUAS CRIAÇÕES", bg='white', fg="red",  font=bold_font)
+test.pack(ipadx=10, ipady=12)  # Ajuste o ipadx/ipady conforme necessário
+
+# Frame para a direita da tela
+right_frame = tk.Frame(window)
+right_frame.pack(side='right', fill='both', expand=True)
+
+# Outros widgets podem ser adicionados ao right_frame
+
 window.mainloop()
