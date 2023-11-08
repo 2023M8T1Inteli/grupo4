8ef0ed1867040c41f1274b26ab03b9d7aa81de46
Leandro de Oliveira Custódio <99210769+TheLeleo@users.noreply.github.com>
2023-11-08 18:42:25 -0300

    image in user btn

--- src/program/editGame.py
+++ src/program/editGame.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import font
+from PIL import Image, ImageTk
 import customtkinter
 from customtkinter import *
 
@@ -17,19 +18,22 @@ class App(customtkinter.CTk):
         self.header.pack(fill='x')
 
         #Botões do Header
-        self.frame1 = customtkinter.CTkButton(self.header, width=100, height=58, text="HOME", fg_color="#D51130", hover_color="#333")
+        self.frame1 = customtkinter.CTkButton(self.header, width=100, height=58, text="HOME", fg_color="transparent", hover_color="#333")
         self.frame1.grid(column=1)
         self.frame1.place(relx=0.6)
 
-        self.frame2 = customtkinter.CTkButton(self.header, width=100, height=58, text="JOGOS", fg_color="#D51130", hover_color="#333")
+        self.frame2 = customtkinter.CTkButton(self.header, width=100, height=58, text="JOGOS", fg_color="transparent", hover_color="#333")
         self.frame2.grid(column=2)
         self.frame2.place(relx=0.7)
 
-        self.frame3 = customtkinter.CTkButton(self.header, width=100, height=58, text="RELATÓRIOS", fg_color="#D51130", hover_color="#333")
+        self.frame3 = customtkinter.CTkButton(self.header, width=100, height=58, text="RELATÓRIOS", fg_color="transparent", hover_color="#333")
         self.frame3.grid(column=3)
         self.frame3.place(relx=0.8)
 
-        self.frame4 = customtkinter.CTkButton(self.header, width=50, height=50,  text="", fg_color="#D51130", hover_color="#333", corner_radius=100, border_width=.5, border_color="#fff")
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        user_image = customtkinter.CTkImage(Image.open(file_path + "/white_user.png"), size=(26, 26))
+        
+        self.frame4 = customtkinter.CTkButton(self.header, width=50, height=50, fg_color="transparent", text='', hover_color="#333", corner_radius=100, border_width=.5, border_color="#fff", image=user_image)
         self.frame4.grid(column=4)
         self.frame4.place(relx=.92)
 
@@ -61,10 +65,12 @@ class App(customtkinter.CTk):
         self.play_section = customtkinter.CTkFrame(self.content, width=366.66, height=580)
         self.play_section.grid(column=2, row=0)
 
-        self.play_section_header = customtkinter.CTkFrame(self.play_section, height=50, fg_color="#fff", width=366.66, corner_radius=0)
+        self.play_section_header = customtkinter.CTkFrame(self.play_section, height=50, width=366.66, corner_radius=0)
+        self.play_section_header.pack(fill='both')
         self.play_section_header.grid(column=0, row=0)
-
-        #self.play_section_header_btn = customtkinter.CTkButton(self.play_section_header, fg_color="blue", height=20, width=20, corner_radius=0, text="pimba")
+        
+        self.play_section_header_btn = customtkinter.CTkButton(self.play_section_header, width=60, height=40, fg_color="#D51130", text="Criar", corner_radius=20, hover_color="#333")
+        self.play_section_header_btn.grid(column=0, row=0)
 
         self.play_section_sub_header = customtkinter.CTkFrame(self.play_section, height=40, fg_color="#CDCBCB", width=366.66, corner_radius=0)
         self.play_section_sub_header.grid(column=0, row=1)
