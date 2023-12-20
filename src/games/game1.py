b86c230fb213176ff84f51958ee1c1f2d17f5919
Leandro de Oliveira Custódio <99210769+TheLeleo@users.noreply.github.com>
2023-12-20 01:29:19 -0300

    removing blocks

--- src/games/game1.py
+++ src/games/game1.py
@@ -9,18 +9,19 @@ running = True
 font_title = pygame.font.Font(None, 50)
 font_text = pygame.font.Font(None, 36)
 clock = pygame.time.Clock()
-image_path1 = r'/Users/gabrielcarneiro/Documents/GitHub/grupo4/src/games/img/logo_aladdin.png'
+image_path1 = r"C:\Users\Inteli\Downloads\minato.png"
 image1 = pygame.image.load(image_path1).convert()
 image1 = pygame.transform.scale(image1, (550, 300))
 image_rect1 = image1.get_rect(center=screen.get_rect().center)
-image_path2 = r'/Users/gabrielcarneiro/Documents/GitHub/grupo4/src/games/img/Screen Shot 2023-12-18 at 18.33.20.png'
+image_path2 = r"C:\Users\Inteli\Downloads\minato.png"
 image2 = pygame.image.load(image_path2).convert()
 image2 = pygame.transform.scale(image2, (550, 300))
 image_rect2 = image2.get_rect(center=screen.get_rect().center)
-image_path3 = r'/Users/gabrielcarneiro/Documents/GitHub/grupo4/src/games/img/inteli-logo.png'
+image_path3 = r"C:\Users\Inteli\Downloads\minato.png"
 image3 = pygame.image.load(image_path3).convert()
 image3 = pygame.transform.scale(image3, (550, 300))
 image_rect3 = image3.get_rect(center=screen.get_rect().center)
+text = font_text.render("Texto ao pressionar K_DOWN", True, (0, 0, 0))
 while True:
     # Seu código Python continua aqui...
 
@@ -34,6 +35,7 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key ==  pygame.K_DOWN:
                 screen.blit(image2, image_rect2)
+                screen.blit(text, (100, 400))
 
             else:
                 screen.blit(image3, image_rect3)
